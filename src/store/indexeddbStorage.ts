232e27bfd25fa648831a00bc937f9a11c77ea596
PrincyMaheshwari <princymaheshwari069@gmail.com>
2025-12-31 00:47:34 -0500

    Started creating zustand stores

---
+++ src/store/indexeddbStorage.ts
@@ -0,0 +1,42 @@
+import Dexie from "dexie";
+
+type AppStateItem = {
+  key: string; // Key will be the store name
+  value: string; //JSON.stringify of the state
+};
+
+const db = new Dexie("FinanceTrackerDB");
+
+db.version(1).stores({
+  appState: "key",
+});
+
+const appStateTable = db.table<AppStateItem, string>("appState");
+
+export const indexeddbStorage = {
+  getItem: async (key: string): Promise<string | null> => {
+    try {
+      const item = await appStateTable.get(key);
+      return item ? item.value : null; //Return the value if found, otherwise null
+    } catch (error) {
+      console.error("Error getting item from IndexedDB:", error);
+      return null;
+    }
+  },
+  setItem: async (key: string, value: string): Promise<void> => {
+    try {
+      await appStateTable.put({ key, value });
+    } catch (error) {
+      console.error("Error setting item in IndexedDB:", error);
+      throw error;
+    }
+  },
+  removeItem: async (key: string): Promise<void> => {
+    try {
+      await appStateTable.delete(key);
+    } catch (error) {
+      console.error("Error removing item from IndexedDB:", error);
+      throw error;
+    }
+  },
+};
